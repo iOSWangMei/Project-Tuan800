2113b35976031cb688704fa5355f63081f572f2e
iOSWangMei <1315802760@qq.com>
2016-06-24 20:53:49 +0800

    Initial Commit

---
+++ Project-Tuan800/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project-Tuan800
+//
+//  Created by mac on 16/6/24.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
